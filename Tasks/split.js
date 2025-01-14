e6d7e69a1dfcd8f3d559bc3e6c2f781e73061a41
rangetik <dobroiranget@gmail.com>
2025-01-14 19:00:31 +0200

    add 'use strict' and const

--- Tasks/split.js
+++ Tasks/split.js
@@ -1,8 +1,10 @@
 // Refactor following solution
 // Split an array into two parts
 
-let split_Array = (index, array) => {
-  begin = array.slice(0, index);
+'use strict';
+
+const split_Array = (index, array) => {
+  const begin = array.slice(0, index);
   const len = array.length;
   array = array.slice(index, len);
   return [begin, array];
