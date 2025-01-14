894bf7ebd8930d6214b11d94d5fb801bb765bd51
rangetik <dobroiranget@gmail.com>
2025-01-14 21:48:01 +0200

    add 'use strict' and const

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,15 +1,17 @@
 // Refactor following solution
 // Filter array by type name
 
-Filter = (T, t) => {
-  remove = [];
-  for (C of T) {
-    x = T.indexOf(C);
+'use strict';
+
+const Filter = (T, t) => {
+  const remove = [];
+  for (const C of T) {
+    const x = T.indexOf(C);
     if (typeof T[x] !== t) {
       remove.unshift(x);
     }
   }
-  for (x of remove) T.splice(x, 1);
+  for (const x of remove) T.splice(x, 1);
   return T;
 };
 
