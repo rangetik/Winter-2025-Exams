37c3b6192e443fc266a4bb58b38bc340841c0283
rangetik <dobroiranget@gmail.com>
2025-01-14 20:55:35 +0200

    Simplify the function with Object.values and reduce

--- Tasks/count.js
+++ Tasks/count.js
@@ -3,13 +3,10 @@
 
 'use strict';
 
-const count = (obj) => {
-  let sum = 0;
-  for (const key of Object.keys(obj)){
-    const value = obj[key];
-    if (typeof value === 'number') sum += value;
-  }
-  return sum;
-};
+const count = (obj) =>
+  Object.values(obj).reduce(
+    (sum, value) => (typeof value === 'number' ? sum + value : sum),
+    0,
+  );
 
 module.exports = count;
