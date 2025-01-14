658312d314e989838d076c9bdfc00252642f1a07
rangetik <dobroiranget@gmail.com>
2025-01-14 19:06:02 +0200

    Function name changed

--- Tasks/split.js
+++ Tasks/split.js
@@ -3,10 +3,10 @@
 
 'use strict';
 
-const split_Array = (index, array) => {
+const splitArray = (index, array) => {
   const begin = array.slice(0, index);
   const rest = array.slice(index);
   return [begin, rest];
 };
 
-module.exports = split_Array;
+module.exports = splitArray;
