002fa356e087cec6f3879a2c2d98d40a7096f902
rangetik <dobroiranget@gmail.com>
2025-01-14 19:03:57 +0200

    input parameter changes are avoided

--- Tasks/split.js
+++ Tasks/split.js
@@ -5,9 +5,8 @@
 
 const split_Array = (index, array) => {
   const begin = array.slice(0, index);
-  const len = array.length;
-  array = array.slice(index, len);
-  return [begin, array];
+  const rest = array.slice(index);
+  return [begin, rest];
 };
 
 module.exports = split_Array;
